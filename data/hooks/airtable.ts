ef9af54be43e1791a003fd19cc40797f5ab5ef1b
Rishiraj Purohit <12214835+ri5h@users.noreply.github.com>
2022-08-20 18:13:53 +0200

    cleanup

--- data/hooks/airtable.ts
+++ data/hooks/airtable.ts
@@ -5,8 +5,6 @@ const Config = {
     baseId: process.env.AIRTABLE_BASE_ID as string
 }
 
-// var base = new Airtable({ apiKey: 'keyvw8jEKxemKuj0j' }).base('appRWuC5U6rnQSqq5');
-
 const airtable = (table: string) => new Airtable({ apiKey: Config.apiKey }).base(Config.baseId)(table)
 
 export default airtable
